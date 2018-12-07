34e4da0c225519da8d1c6b1e66de51ea1d279063
Paul Vangelakos <paulvangelakos@gmail.com>
2018-12-07 11:02:09 -0700

    Adding dev dependencies

---
+++ test/Game-test.js
@@ -0,0 +1,11 @@
+const chai = require('chai');
+const expect = chai.expect;
+const Game = require('../js/Game.js');
+
+
+
+describe('Game', function () {
+  it('should return true', function () {
+    expect(true).to.equal(true);
+  });
+});
