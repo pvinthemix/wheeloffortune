b7e8c78bbc8efb2d3db554c541e6104adf70fceb
Paul Vangelakos <paulvangelakos@gmail.com>
2018-12-07 13:37:04 -0700

    Added a test for player

--- test/Player-test.js
+++ test/Player-test.js
@@ -1,3 +1,11 @@
 const chai = require('chai');
 const expect = chai.expect;
 const Player = require('../js/Player.js');
+
+
+describe('Player', function () {
+  var player = new Player('Lee');
+  it('it should have a name', function () {
+    expect(player.name).to.equal('Lee');
+  });
+});
