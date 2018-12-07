99f14ec66a58ee15ffc33673658f573a42a9f000
Paul Vangelakos <paulvangelakos@gmail.com>
2018-12-06 18:21:49 -0700

    Instaniated the puzzle and wheel classes

--- js/Game.js
+++ js/Game.js
@@ -2,16 +2,13 @@ class Game {
   constructor(player1, player2, player3) {
     this.players = this.createPlayers(player1, player2, player3);
     this.round =  1;
-    this.fivePuzzles = null;
-    this.wheel = [];
+    this.fivePuzzles = this.generateFivePuzzles();
+    this.wheel = this.generateWheel();
     this.boundRound = false;
     this.playerIndex = 0;
     // this.turn = this.player[this.playerIndex];
   }
 
-  startGame() {
-
-  }
 
   resetGame() {
 
@@ -26,14 +23,18 @@ class Game {
     this.changeTurn();
   }
 
-  generateFivePuzzles() {
-    //Chooses 5 random puzzles
-    //iterate over all of the banks and make them into one large array
-    //generate a random number between zero and the length of the indexes 
-    // conditional if we run into the same index to generate a new one
-    //then generate 5 new puzzle classes based on that
-    //pass into puzzle constructor 
+  generateWheel() {
+    const wheelOne = new Wheel();
+    return [wheelOne]
+  }
 
+  generateFivePuzzles() {
+    const roundOne = new Puzzle(1);
+    const roundTwo = new Puzzle(2);
+    // const roundThree = new Puzzle(3);
+    // const roundFour = new Puzzle(4);
+    // const roundFive = new Puzzle(5);
+    return [roundOne, roundTwo]
   }
 
   createPlayers(player1, player2, player3) {
