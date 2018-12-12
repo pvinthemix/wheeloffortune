dc410ad4ac7458b10cbf7616ed10672e5a5f7448
Heather Hartley <hlhartley@gmail.com>
2018-12-11 23:01:29 -0700

    Adds bonusClass.js

--- js/BonusWheel.js
+++ js/BonusWheel.js
@@ -1,11 +1,18 @@
-// class BonusWheel extends Wheel {
-//   constructor() {
-//     super();
-//     this.bonusPlayer = null;
-//     this.bonusWheelValue = null;
-//   }
-// }
+class BonusWheel extends Wheel {
+  constructor() {
+    super();
+    this.bonusSpinValues = ['FERRARI', '$30,000', 'BOAT', 'TESLA', 'TRIP TO HAWAII', '$50,000'];
+    this.bonusPlayer = null;
+    this.bonusWheelValue = generateCurrentBonusWheelSpinValue();
+  }
 
-// if (typeof module !== 'undefined') {
-//   module.exports = BonusWheel;
-// }
+generateCurrentBonusWheelSpinValue() {
+    const randomIndex = Math.floor(Math.random() * 6);
+    this.bonusWheelValue = this.bonusSpinValues[randomIndex];
+    return this.bonusWheelValue;
+  }
+}
+
+if (typeof module !== 'undefined') {
+  module.exports = BonusWheel;
+}
