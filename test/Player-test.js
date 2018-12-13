93379cd262912863289c2e781d8779af8ffea65f
Paul Vangelakos <paulvangelakos@gmail.com>
2018-12-13 15:51:21 -0700

    Added lots of tests to the classes and bonus class some with spies

--- test/Player-test.js
+++ test/Player-test.js
@@ -1,21 +1,38 @@
 const chai = require('chai');
 const expect = chai.expect;
 const Player = require('../js/Player.js');
+global.Game = require('../js/Game.js');
 
 
 describe('Player', function () {
+  let player;
+  beforeEach (function () {
+    player = new Player('Lee');
+  });
+
   it('it should have a name', function () {
-    var player = new Player('Lee');
     expect(player.name).to.equal('Lee');
   });
 
   it('should start with a bank account of zero', function() {
-    var player = new Player('Lee');
     expect(player.grandTotal).to.equal(0)
   })
 
   it('should start off with a wallet of zero', function() {
-    var player = new Player('Lee');
     expect(player.score).to.equal(0)
+  })
+
+  it('should increase the current players score', function() {
+    var game = new Game();
+    var wheel = new Wheel();
+    wheel.currentSpinValue = 100;
+    player.increaseCurrentPlayerScore(game, wheel);
+    expect(player.score).to.equal(100)
+  })
+
+  it('should decreae the current players score', function () {
+    var game = new Game();
+    player.decreaseCurrentPlayerScore(game);
+    expect(player.score).to.equal(-100)
   })
 });
--- test/Wheel-test.js
+++ test/Wheel-test.js
@@ -1,18 +1,44 @@
 const chai = require('chai');
 const expect = chai.expect;
 const Wheel = require('../js/Wheel.js');
+global.Game = require('../js/Game.js');
+const spies = require('chai-spies');
+chai.use(spies);
+global.domUpdates = require('../js/Domupdates.js')
 
 describe('Wheel', function () {
+  var wheel;
+  beforeEach(function () {
+    chai.spy.on(global.domUpdates, ['showPlayersTurn', 'showPlayerScore'], () => true);
+    wheel = new Wheel();
+  });
+
+  afterEach(() => {
+    chai.spy.restore(global.domUpdates)
+  })
   it('it should have a spin value of the current spin', function () {
-    var wheel = new Wheel();
     wheel.currentSpinValue = 600;
     expect(wheel.currentSpinValue).to.equal(600);
   });
 
-  it('it should generate an array of 6 random spin values', function () {
-    var wheel = new Wheel();
-
+  it('it should generate an array of 6 spin values', function () {
     wheel.generateSpinValues();
     expect(wheel.spinValues.length).to.equal(6);
   });
+
+  it('it should check for the spin value of Bankrupt', function () {
+    var game = new Game();
+    wheel.currentSpinValue = 'BANKRUPT';
+    wheel.checkForBankrupt(game);
+    expect(wheel.currentSpinValue).to.equal('BANKRUPT');
+    expect(domUpdates.showPlayerScore).to.have.been.called(1);
+  })
+
+  it('it should check for the spin value of lose turn', function() {
+    var game = new Game();
+    wheel.currentSpinValue = 'LOSE A TURN';
+    wheel.checkForLoseTurn(game);
+    expect(wheel.currentSpinValue).to.equal('LOSE A TURN');
+    expect(domUpdates.showPlayersTurn).to.have.been.called(1);
+  })
 });
